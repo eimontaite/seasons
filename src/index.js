6676318a0615d27dd9ed3faeaaa37efdb0b7e90f
Aiste Eimontaite <eimontaite.aiste@gmail.com>
2020-06-16 21:27:29 +0300

    Add mock SeasonDisplay; obtain user's latitude

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    return <div>Hi there!</div>
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { lat: null };
+    }
+
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({ lat: position.coords.latitude })
+            },
+            (error) => console.log(error)
+        );
+        return <div>Latitude: {this.state.lat}</div>
+    }
 }
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
