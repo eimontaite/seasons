e10b1e84f2349b3f545554188213a9c2012ce3f5
Aiste Eimontaite <eimontaite.aiste@gmail.com>
2020-06-17 12:01:07 +0300

    Move conditional logic outside of render()

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ class App extends React.Component {
         );
     }
 
-    render() {
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>
         }
@@ -26,6 +26,14 @@ class App extends React.Component {
         }
         
         return <div><Spinner message="Please accept location request"/></div>
+    }
+
+    render() {
+        return(
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        )
     }
 }
 
